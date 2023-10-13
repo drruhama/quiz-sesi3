e5ad8d600f457ec686d9bde9ae87347f57c304e3
drruhama <146839610+drruhama@users.noreply.github.com>
2023-10-13 23:27:53 +0700

    Add files via upload

--- latihan-perulangan/main.go
+++ latihan-perulangan/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var hasil = ""
+	for i := 0; i < 5; i++ {
+		for y := 5; y > i; y-- {
+			hasil += "*"
+		}
+		hasil += "\n"
+	}
+	fmt.Println(hasil)
 	y := "*"
 
 	for x := "*"; x <= "*****"; x = x + y {
 		fmt.Println(x)
 	}
+
 }
